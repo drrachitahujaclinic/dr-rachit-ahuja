f2863a0a6d49b48317c44ccf632ee97c46f26c32
Manik Sharma <maniksharma.rke@gmail.com>
2025-12-14 03:11:52 +0530

    file

---
+++ app/cancellation-and-refund.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CancellationAndRefund = () => {
+  return (
+    <div>
+      DR. RACHIT AHUJA believes in helping its customers as far as possible, and has therefore a liberal cancellation policy. Under this policy: Cancellations will be considered only if the request is made within Not Applicable of placing the order. However, the cancellation request may not be entertained if the orders have been communicated to the vendors/merchants and they have initiated the process of shipping them. DR. RACHIT AHUJA does not accept cancellation requests for perishable items like flowers, eatables etc. However, refund/replacement can be made if the customer establishes that the quality of product delivered is not good. In case of receipt of damaged or defective items please report the same to our Customer Service team. The request will, however, be entertained once the merchant has checked and determined the same at his own end. This should be reported within Not Applicable of receipt of the products. In case you feel that the product received is not as shown on the site or as per your expectations, you must bring it to the notice of our customer service within Not Applicable of receiving the product. The Customer Service Team after looking into your complaint will take an appropriate decision. In case of complaints regarding products that come with a warranty from manufacturers, please refer the issue to them. In case of any Refunds approved by the DR. RACHIT AHUJA, it’ll take Not Applicable for the refund to be processed to the end customer.
+    </div>
+  )
+}
+
+export default CancellationAndRefund;
